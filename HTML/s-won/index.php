742906681e6f901597bcab11a4533d52403b2af1
huqbgxixb <huqbgxixb@gmail.com>
2014-07-09 23:41:27 +0900

    수

--- HTML/s-won/index.php
+++ HTML/s-won/index.php
@@ -10,6 +10,14 @@
 			line-height: 150%;
 			text-align: center;
 		}
+		#editor { 
+	        margin: 0;
+	        position: absolute;
+	        top: 0;
+	        bottom: 0;
+	        left: 0;
+	        right: 0;
+	    }
 		textarea {
 			width: 95%;
 			margin: 0 auto;
@@ -25,51 +33,51 @@
 			line-height: 120%;
 		}
 		.button {
-		border: 2px solid #0a3c59;
-		background: #3e779d;
-		background: -webkit-gradient(linear, left top, left bottom, from(#65a9d7), to(#3e779d));
-		background: -webkit-linear-gradient(top, #65a9d7, #3e779d);
-		background: -moz-linear-gradient(top, #65a9d7, #3e779d);
-		background: -ms-linear-gradient(top, #65a9d7, #3e779d);
-		background: -o-linear-gradient(top, #65a9d7, #3e779d);
-		background-image: -ms-linear-gradient(top, #65a9d7 0%, #3e779d 100%);
-		padding: 5px 10px;
-		-webkit-border-radius: 6px;
-		-moz-border-radius: 6px;
-		border-radius: 6px;
-		-webkit-box-shadow: rgba(255,255,255,0.4) 0 1px 0, inset rgba(255,255,255,0.4) 0 1px 0;
-		-moz-box-shadow: rgba(255,255,255,0.4) 0 1px 0, inset rgba(255,255,255,0.4) 0 1px 0;
-		box-shadow: rgba(255,255,255,0.4) 0 1px 0, inset rgba(255,255,255,0.4) 0 1px 0;
-		text-shadow: #7ea4bd 0 1px 0;
-		color: #06426c;
-		font-size: 22px;
-		font-family: helvetica, serif;
-		text-decoration: none;
-		vertical-align: middle;
+			border: 2px solid #0a3c59;
+			background: #3e779d;
+			background: -webkit-gradient(linear, left top, left bottom, from(#65a9d7), to(#3e779d));
+			background: -webkit-linear-gradient(top, #65a9d7, #3e779d);
+			background: -moz-linear-gradient(top, #65a9d7, #3e779d);
+			background: -ms-linear-gradient(top, #65a9d7, #3e779d);
+			background: -o-linear-gradient(top, #65a9d7, #3e779d);
+			background-image: -ms-linear-gradient(top, #65a9d7 0%, #3e779d 100%);
+			padding: 5px 10px;
+			-webkit-border-radius: 6px;
+			-moz-border-radius: 6px;
+			border-radius: 6px;
+			-webkit-box-shadow: rgba(255,255,255,0.4) 0 1px 0, inset rgba(255,255,255,0.4) 0 1px 0;
+			-moz-box-shadow: rgba(255,255,255,0.4) 0 1px 0, inset rgba(255,255,255,0.4) 0 1px 0;
+			box-shadow: rgba(255,255,255,0.4) 0 1px 0, inset rgba(255,255,255,0.4) 0 1px 0;
+			text-shadow: #7ea4bd 0 1px 0;
+			color: #06426c;
+			font-size: 22px;
+			font-family: helvetica, serif;
+			text-decoration: none;
+			vertical-align: middle;
 		}
 		.button:hover {
-		border: 2px solid #0a3c59;
-		text-shadow: #1e4158 0 1px 0;
-		background: #3e779d;
-		background: -webkit-gradient(linear, left top, left bottom, from(#65a9d7), to(#3e779d));
-		background: -webkit-linear-gradient(top, #65a9d7, #3e779d);
-		background: -moz-linear-gradient(top, #65a9d7, #3e779d);
-		background: -ms-linear-gradient(top, #65a9d7, #3e779d);
-		background: -o-linear-gradient(top, #65a9d7, #3e779d);
-		background-image: -ms-linear-gradient(top, #65a9d7 0%, #3e779d 100%);
-		color: #fff;
+			border: 2px solid #0a3c59;
+			text-shadow: #1e4158 0 1px 0;
+			background: #3e779d;
+			background: -webkit-gradient(linear, left top, left bottom, from(#65a9d7), to(#3e779d));
+			background: -webkit-linear-gradient(top, #65a9d7, #3e779d);
+			background: -moz-linear-gradient(top, #65a9d7, #3e779d);
+			background: -ms-linear-gradient(top, #65a9d7, #3e779d);
+			background: -o-linear-gradient(top, #65a9d7, #3e779d);
+			background-image: -ms-linear-gradient(top, #65a9d7 0%, #3e779d 100%);
+			color: #fff;
 		}
 		.button:active {
-		text-shadow: #1e4158 0 1px 0;
-		border: 2px solid #0a3c59;
-		background: #65a9d7;
-		background: -webkit-gradient(linear, left top, left bottom, from(#3e779d), to(#3e779d));
-		background: -webkit-linear-gradient(top, #3e779d, #65a9d7);
-		background: -moz-linear-gradient(top, #3e779d, #65a9d7);
-		background: -ms-linear-gradient(top, #3e779d, #65a9d7);
-		background: -o-linear-gradient(top, #3e779d, #65a9d7);
-		background-image: -ms-linear-gradient(top, #3e779d 0%, #65a9d7 100%);
-		color: #fff;
+			text-shadow: #1e4158 0 1px 0;
+			border: 2px solid #0a3c59;
+			background: #65a9d7;
+			background: -webkit-gradient(linear, left top, left bottom, from(#3e779d), to(#3e779d));
+			background: -webkit-linear-gradient(top, #3e779d, #65a9d7);
+			background: -moz-linear-gradient(top, #3e779d, #65a9d7);
+			background: -ms-linear-gradient(top, #3e779d, #65a9d7);
+			background: -o-linear-gradient(top, #3e779d, #65a9d7);
+			background-image: -ms-linear-gradient(top, #3e779d 0%, #65a9d7 100%);
+			color: #fff;
 		}
 	</style>
 
@@ -88,14 +96,13 @@
 
 
 ?>
+
 <form action="result.php" method="post">
 	userid : <input type="text" name="username" size="30" maxlength="20" value=<?php echo '"'.$userName.'"'; ?>><br>
 	<textarea name="sourcecode"　wrap="off"><?php echo urldecode($sourceCode); ?></textarea><br>
-	<input type="submit" value="결과보기">
-	<input type="reset" value="내용 초기화">
+	<input class="button" type="submit" value="결과보기">
+	<input class="button" type="reset" value="내용 초기화">
 </form>
-
-<a href='#' class='button'>TestButton</a>
 
 </body>
 </html>
